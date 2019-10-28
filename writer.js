b8a75e6c45c8f5476b458c66022a769b04842d83
JOHN NELSON <JohnNelson4850@gmail.com>
2019-10-28 14:33:26 -0700

    working linter

--- writer.js
+++ writer.js
@@ -1,11 +1,15 @@
-fs = require('fs');
+const fs = require('fs');
 
 function write(str, path) {
-
+  fs.writeFile(path, str, (err) => {
+    if(err) throw err;
+  });
 }
 
 function writeJSON(obj, path) {
-
+  fs.writeFile(path, obj, JSON, (err) => {
+    if(err) throw err;
+  });
 }
 
 module.exports = write, writeJSON;
